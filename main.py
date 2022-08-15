ffb7fe896f8b728d030dce7ddef247df70b15380
Jae Williams <97705866+JavvyTheBuilder@users.noreply.github.com>
2022-08-14 20:50:43 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+print("This is the Kilometers to Miles Calculator!")
+
+#kilometer = float(input("How many Kilometers are you traveling? \n "))
+#mile = kilometer * 0.621
+
+mile = float(input("How many miles are you traveling? \n"))
+
+kilometer = 1.60934 * mile
+
+#print(mile)
+print(round(kilometer, 2))
